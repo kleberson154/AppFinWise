c52b17895ef658d7ad3837e328830cf32f8853da
kleberson154 <kleberson55@hotmail.com>
2025-06-30 14:46:56 -0300

    Criando a Main Activity

--- app/src/main/java/com/kleberson/finwise/view/MainActivity.kt
+++ app/src/main/java/com/kleberson/finwise/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.kleberson.finwise.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnAddSaldo = findViewById<Button>(R.id.buttonAddSaldo)
+
+        btnAddSaldo.setOnClickListener {
+            startActivity(Intent(this, AddBalanceActivity::class.java))
         }
     }
 }
