d6b6b67fd3f56c7c8fd975e60b94159803bd7cb8
kleberson154 <kleberson55@hotmail.com>
2025-07-01 14:04:50 -0300

    Adicionando o controller para chamar a funcao addBalance

--- app/src/main/java/com/kleberson/finwise/view/AddBalanceActivity.kt
+++ app/src/main/java/com/kleberson/finwise/view/AddBalanceActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.kleberson.finwise.R
+import com.kleberson.finwise.controller.UserController
 
 class AddBalanceActivity: AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -16,13 +17,28 @@ class AddBalanceActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_balance)
+        val emailUser = intent.getStringExtra("email")
 
         val btnBalance = findViewById<Button>(R.id.buttonAddActivity)
         val inputBalance = findViewById<EditText>(R.id.editTextInputBalance)
 
+        val userController = UserController(this)
+
         btnBalance.setOnClickListener{
+            if (inputBalance.text.isEmpty()) {
+                Toast.makeText(this, "Por favor, insira um valor válido.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (emailUser != null) {
+                userController.addBalance(this, emailUser, inputBalance.text.toString().toDouble())
+            }else{
+                Toast.makeText(this, "Usuário não encontrado.", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+
             Toast.makeText(this, "Saldo adicionado com sucesso!", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java).putExtra("email", emailUser))
             finish()
         }
     }
