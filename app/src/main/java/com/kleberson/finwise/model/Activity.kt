966fbd90c805578e950353023654454e888929aa
kleberson154 <kleberson55@hotmail.com>
2025-07-01 16:26:22 -0300

    Acrescentando o id no model Activity

--- app/src/main/java/com/kleberson/finwise/model/Activity.kt
+++ app/src/main/java/com/kleberson/finwise/model/Activity.kt
@@ -1,9 +1,9 @@
 package com.kleberson.finwise.model
 
-import java.sql.Date
+import java.util.Date
 
-class Activity(val name: String, val category: String, val type: String, val price: Double, val date: Date ) {
+data class Activity(val id: Int, val name: String, val category: String, val type: String, val price: Double, val date: Date) {
     override fun toString(): String {
-        return "Activity(name='$name', category='$category', type='$type', price=$price, date=$date)"
+        return "Activity(id=$id, name='$name', category='$category', type='$type', price=$price, date=$date)"
     }
 }
